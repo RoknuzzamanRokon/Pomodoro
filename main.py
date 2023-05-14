9db0c9a75f59a53631c8890eb19f3cbf8c104653
Rokon Ron <rokon.raz@gmail.com>
2023-05-14 08:30:50 +0600

    Give color in canvas create a clock use canvas.create_text()

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+YELLOW = "#f7f5cd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -18,11 +18,13 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
 
-canvas = Canvas(width="200", height="224")
+canvas = Canvas(width="200", height="224", bg=YELLOW, highlightthickness=0)
 image_canvas = PhotoImage(file="tomato.png")
-canvas.create_image(102, 112, image=image_canvas)
+canvas.create_image(100, 112, image=image_canvas)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, 'bold'))
 canvas.pack()
 
 window.mainloop()
