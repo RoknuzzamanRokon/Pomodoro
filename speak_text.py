5d7ab0616058b0a2b9c3a8bcd189e650df0f93c0
Rokon Ron <rokon.raz@gmail.com>
2023-05-18 13:22:06 +0600

    Create a another function for 20m break sound

--- speak_text.py
+++ speak_text.py
@@ -8,3 +8,11 @@ def take_5_minutes_rest():
 
     engine.runAndWait()
 
+
+def take_20_minutes_rest():
+    engine = pyttsx3.init()
+
+    engine.say("Naw you will take twenty minutes rest.")
+
+    engine.runAndWait()
+
